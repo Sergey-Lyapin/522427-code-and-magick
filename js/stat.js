85149556d171f132ea29959b6e372b70553eb639
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-07 18:59:14 +0300

    Исправления2

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
@@ -8,7 +7,6 @@ var GAP = 10;
 var START_GAP = 55;
 var TEXT_WIDTH = 40;
 var TEXT_GAP = 250;
-var TEXT_HEIGHT = 20;
 var RECT_GAP = 240;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
@@ -70,19 +68,16 @@ window.renderStatistics = function (ctx, players, times) {
   var maxTime = getMaxElement(times);
   for (var i = 0; i < players.length; i++) {
     var randomOpacity = Math.random().toFixed(2);
-    if (players[i] === 'Вы' ) {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)'; 
-    }
-    else {
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    }else {
       ctx.fillStyle = 'rgba(0, 77, 255, ' + randomOpacity + ')';
     }
-    ctx.fillRect(CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + RECT_GAP, BAR_WIDTH, -(barHeight * times[i]) / maxTime );
+    ctx.fillRect(CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + RECT_GAP, BAR_WIDTH, -(barHeight * times[i]) / maxTime);
   }
-  
-  for (i = 0; i < players.length; i++) { 
+  for (i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + TEXT_GAP);
     ctx.fillText(Math.round(times[i]), CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + (CLOUD_HEIGHT - (barHeight * times[i]) / maxTime) - TIME_GAP);
-    }
-  
+  }  
 };
