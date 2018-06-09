4a93acc714e78d4c8f315239d04f224a715e7226
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-09 03:48:31 +0300

    Снова исправления

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var coordinatesCloud = [[120, 500, 520, 520, 500, 120, 100, 100, 120], [10, 10, 30, 260, 280, 280, 260, 30, 10]];
+var coordinatesShadow = [[130, 510, 530, 530, 510, 130, 110, 110, 130], [20, 20, 40, 270, 290, 290, 270, 40, 20]];
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var HEADER_X = 140;
+var HEADER_Y = 30;
 var GAP = 10;
 var START_GAP = 55;
 var TEXT_WIDTH = 40;
@@ -11,42 +15,22 @@ var RECT_GAP = 240;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var TIME_GAP = 50;
-var barHeight = 150;
-var POINT_1X = 120;
-var POINT_1Y = 10;
-var POINT_2X = 500;
-var POINT_2Y = 10;
-var POINT_3X = 520;
-var POINT_3Y = 30;
-var POINT_4X = 520;
-var POINT_4Y = 260;
-var POINT_5X = 500;
-var POINT_5Y = 280;
-var POINT_6X = 120;
-var POINT_6Y = 280;
-var POINT_7X = 100;
-var POINT_7Y = 260;
-var POINT_8X = 100;
-var POINT_8Y = 30;
-var POINT_9X = 120;
-var POINT_9Y = 10;
+var BAR_HEIGHT = 150;
 
-var renderCloud = function (ctx, point1x, point1y, point2x, point2y, point3x, point3y, point4x, point4y, point5x, point5y, point6x, point6y, point7x, point7y, point8x, point8y, point9x, point9y, color, strokeColor) {
+var renderCloud = function (ctx, coordinates, color, strokeColor) {
   ctx.fillStyle = color;
   ctx.strokeStyle = strokeColor;
   ctx.beginPath();
-  ctx.moveTo(POINT_1X, POINT_1Y);
-  ctx.lineTo(POINT_2X, POINT_2Y);
-  ctx.lineTo(POINT_3X, POINT_3Y);
-  ctx.lineTo(POINT_4X, POINT_4Y);
-  ctx.lineTo(POINT_5X, POINT_5Y);
-  ctx.lineTo(POINT_6X, POINT_6Y);
-  ctx.lineTo(POINT_7X, POINT_7Y);
-  ctx.lineTo(POINT_8X, POINT_8Y);
-  ctx.lineTo(POINT_9X, POINT_9Y);
+  ctx.moveTo(coordinates[0][0], coordinates[1][0]);
+
+  for (var i = 1; i < coordinates[0].length; i++) {
+    ctx.lineTo(coordinates[0][i], coordinates[1][i]);
+  }
+
   ctx.stroke();
   ctx.fill();
-};
+}
+
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -61,14 +45,13 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, POINT_1X + GAP, POINT_1Y + GAP, POINT_2X + GAP, POINT_2Y + GAP, POINT_3X + GAP, POINT_3Y + GAP, POINT_4X + GAP, POINT_4Y + GAP, POINT_5X + GAP, POINT_5Y + GAP, POINT_6X + GAP, POINT_6Y + GAP, POINT_7X + GAP, POINT_7Y + GAP, POINT_8X + GAP, POINT_8Y + GAP, POINT_9X + GAP, POINT_9Y + GAP, 'rgba(0, 0, 0, 0.3)', 'transparent');
-
-  renderCloud(ctx, POINT_1X, POINT_1Y, POINT_2X, POINT_2Y, POINT_3X, POINT_3Y, POINT_4X, POINT_4Y, POINT_5X, POINT_5Y, POINT_6X, POINT_6Y, POINT_7X, POINT_7Y, POINT_8X, POINT_8Y, POINT_9X, POINT_9Y, '#fff', 'transparent');
+  renderCloud(ctx, coordinatesShadow, 'rgba(0, 0, 0, 0.3)', 'transparent');
+  renderCloud(ctx, coordinatesCloud, '#fff', 'transparent');
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили! Список результатов:', 140, 30);
+  ctx.fillText('Ура вы победили! Список результатов:', HEADER_X, HEADER_Y);
 
   var maxTime = getMaxElement(times);
 
@@ -81,9 +64,9 @@ window.renderStatistics = function (ctx, players, times) {
       ctx.fillStyle = 'rgba(0, 77, 255, ' + randomOpacity + ')';
     }
 
-    ctx.fillRect(CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + RECT_GAP, BAR_WIDTH, -(barHeight * times[i]) / maxTime);
+    ctx.fillRect(CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + RECT_GAP, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / maxTime);
     ctx.fillStyle = '#000';
     ctx.fillText(players[i], CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + TEXT_GAP);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + (CLOUD_HEIGHT - (barHeight * times[i]) / maxTime) - TIME_GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + START_GAP + (TEXT_WIDTH + BAR_GAP) * i, CLOUD_Y + (CLOUD_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime) - TIME_GAP);
   }
 };
