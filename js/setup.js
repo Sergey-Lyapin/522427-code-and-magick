e13ea59879e9ec79e706e46e2fe793d32b7b607e
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-12 20:49:08 +0300

    Похожие волшебники

---
+++ js/setup.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var dataWizards = {
+  numberOfWizards: 4,
+  names: ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '],
+  surnames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
+};
+
+var userDialog = document.querySelector('.setup');
+var setupSimilarWizards = document.querySelector('.setup-similar');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+openPopup();
+renderWizards();
+
+function generateWizards() {
+  var wizards = [];
+  for (var i = 0; i < dataWizards.numberOfWizards; i++) {
+    wizards.push({
+      name: getRandomElement(dataWizards.names) + getRandomElement(dataWizards.surnames),
+      coatColor: getRandomElement(dataWizards.coatColor),
+      eyesColor: getRandomElement(dataWizards.eyesColor)
+    });
+  }
+  return wizards;
+};
+
+function renderWizard(wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+function renderWizards() {
+  var similarWizards = generateWizards();
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < similarWizards.length; i++) {
+    fragment.appendChild(renderWizard(similarWizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+
+function getRandomElement(array) {
+  for (var i = 0; i < array.length; i++) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    var randomElement = array[randomIndex];
+  }
+  return randomElement;
+};
+
+
+function openPopup() {
+  userDialog.classList.remove('hidden');
+  setupSimilarWizards.classList.remove('hidden');
+};
+
