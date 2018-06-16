9bf5855ceccf75c86b7a919594bb05fdf796348d
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-16 23:41:07 +0300

    Задание по событиям

--- js/setup.js
+++ js/setup.js
@@ -68,3 +68,74 @@ function openPopup() {
   setupSimilarWizards.classList.remove('hidden');
 }
 
+// Задание по 4 лекции:
+
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = document.querySelector('.setup-close');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
+var setupUserName = document.querySelector('.setup-user-name');
+var wizardCoat = setup.querySelector('.wizard-coat');
+var wizardEyes = setup.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+var onCoatClick = function() {
+
+  wizardCoat.style.fill = getRandomElement(COAT_COLORS);
+
+}
+
+var onEyesClick = function() {
+
+  wizardEyes.style.fill = getRandomElement(EYES_COLORS);
+
+}
+
+var onFireballClick = function() {
+
+  wizardFireball.style.backgroundColor = getRandomElement(FIREBALL_COLOR);
+
+}
+
+var onPopupEscPress = function(evt) {
+
+  if ((evt.keyCode === ESC_KEYCODE) && (setupUserName != document.activeElement)) {
+    closePopup();
+  }
+};
+
+var onSetupCloseEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+}
+
+var onSetupOpenEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+}
+
+var openPopup = function() {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', openPopup);
+setupClose.addEventListener('click', closePopup);
+setupClose.addEventListener('keydown', onSetupCloseEnterPress);
+setupOpenIcon.addEventListener('keydown', onSetupOpenEnterPress);
+wizardCoat.addEventListener('click', onCoatClick);
+wizardEyes.addEventListener('click', onEyesClick);
+wizardFireball.addEventListener('click', onFireballClick);
