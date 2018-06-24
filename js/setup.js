bcb904d4e9521bead0ae2b760dd5b01812f9c7cd
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-06-24 19:32:29 +0300

    Перемещение диалогового окна

--- js/setup.js
+++ js/setup.js
@@ -132,6 +132,9 @@ var openPopup = function () {
 
 var closePopup = function () {
   setup.classList.add('hidden');
+  var setupPopup = document.querySelector('.setup');
+  setup.style.top = '80px';
+  setup.style.left = '50%';
   document.removeEventListener('keydown', onPopupEscPress);
 };
 
@@ -142,3 +145,62 @@ setupOpenIcon.addEventListener('keydown', onSetupOpenEnterPress);
 wizardCoat.addEventListener('click', onCoatClick);
 wizardEyes.addEventListener('click', onEyesClick);
 wizardFireball.addEventListener('click', onFireballClick);
+
+// Перемещение диалогового окна
+
+(function () {
+
+  var setupDialogElement = document.querySelector('.setup');
+  var dialogHandler = setupDialogElement.querySelector('.upload');
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + 'px';
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + 'px';
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (evt) {
+          evt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault)
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+
+})();
