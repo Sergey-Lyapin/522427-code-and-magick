4bc1c0c29e38ca24f64f9c348970c69a33163e0a
Sergey-Lyapin <sergeyl9pin@gmail.com>
2018-07-01 16:33:17 +0300

    Задание по работе с сетью

---
+++ js/backend.js
@@ -0,0 +1,51 @@
+window.backend = (function () {
+
+  return {
+    load: function (onLoad, onError) {
+      var URL = 'https://js.dump.academy/code-and-magick/data';
+
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.open('GET', URL);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка, попробуйте снова');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 15000;
+
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var URL = 'https://js.dump.academy/code-and-magick';
+
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        onLoad(xhr.response);
+      });
+
+      xhr.addEventListener('error', function () {
+        onError(xhr.status + ', ' + xhr.statusText);
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    }
+  };
+})();
--- js/setup.js
+++ js/setup.js
@@ -1,59 +1,60 @@
 'use strict';
 (function () {
-  var DATA_WIZARDS = {
-    numberOfWizards: 4,
-    names: ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '],
-    surnames: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-    coatColor: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-    eyesColor: ['black', 'red', 'blue', 'yellow', 'green']
-  };
 
   var userDialog = document.querySelector('.setup');
   var setupSimilarWizards = document.querySelector('.setup-similar');
   var similarListElement = userDialog.querySelector('.setup-similar-list');
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+  var form = userDialog.querySelector('.setup-wizard-form');
 
   openThePopup();
-  renderWizards();
 
-  function generateWizards() {
-    var wizards = [];
-
-    for (var i = 0; i < DATA_WIZARDS.numberOfWizards; i++) {
-
-      wizards.push({
-        name: window.getRandomElement(DATA_WIZARDS.names) + window.getRandomElement(DATA_WIZARDS.surnames),
-        coatColor: window.getRandomElement(DATA_WIZARDS.coatColor),
-        eyesColor: window.getRandomElement(DATA_WIZARDS.eyesColor)
-      });
-
-    }
-
-    return wizards;
-  }
 
   function renderWizard(wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   }
 
-  function renderWizards() {
-    var similarWizards = generateWizards();
+  var successHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < similarWizards.length; i++) {
-      fragment.appendChild(renderWizard(similarWizards[i]));
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
     }
-
     similarListElement.appendChild(fragment);
-  }
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: pink;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '35px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
 
   function openThePopup() {
     userDialog.classList.remove('hidden');
     setupSimilarWizards.classList.remove('hidden');
   }
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function (response) {
+      userDialog.classList.add('hidden');
+    }, function (errorMessage) {
+      console.log(errorMessage);
+    });
+    evt.preventDefault();
+  });
 })();
